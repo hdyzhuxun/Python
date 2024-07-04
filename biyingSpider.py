ee204f3fb97318fc2b0755ae7d39061cb1f79a88
hdyzhuxun <33113701+hdyzhuxun@users.noreply.github.com>
2024-07-04 23:34:40 +0800

    Update biyingSpider.py

--- biyingSpider.py
+++ biyingSpider.py
@@ -1,15 +1,15 @@
-import requests
-import re
-import time
-local = time.strftime("%Y.%m.%d")
-url = 'http://cn.bing.com/'
-con = requests.get(url)
-content = con.text
-reg = r"(az/hprichbg/rb/.*?.jpg)"
-a = re.findall(reg, content, re.S)[0]
-print(a)
+进口要求
+进口关于
+进口时间
+本地=时间。strftime(" %Y.%m.%d ")
+url =' http://cn.bing.com/'
+con =请求。得到(全球资源定位器(Uniform Resource Locator))
+内容= con。文本
+reg =r"(az/hprichbg/rb/。*?。jpg)"
+a = re。芬达尔(注册，内容，回复。S)[0]
+打印(a)
 picUrl = url + a
-read = requests.get(picUrl)
-f = open('%s.jpg' % local, 'wb')
-f.write(read.content)
-f.close()
+读取=请求。得到(皮库尔)
+f =打开(%s.jpg '%本地，wb ')
+f.写(阅读。内容)
+f.关闭()
